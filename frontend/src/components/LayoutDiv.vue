09a9fa7952499d1f936cdb94f9aa0728216df3d8
Shuoni Xu <sharonxu1030@outlook.com>
2023-10-22 21:26:30 -0400

    feature-widget lists and header

--- frontend/src/components/LayoutDiv.vue
+++ frontend/src/components/LayoutDiv.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container">
+    <HeaderNav> </HeaderNav>
+    <WidgetList></WidgetList>
     <slot></slot>
   </div>
 </template>
 
 <script>
+import HeaderNav from "./HeaderNav.vue"
+import WidgetList from "./WidgetList.vue"
 export default {
   name: "LayoutDiv",
+  components: {
+    HeaderNav,
+    WidgetList
+  }
 };
 </script>
---
+++ frontend/src/components/WidgetList.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="home">
+      <div class="p-4">
+        <div class="row">
+          <div class="col-md-2">
+            <router-link
+              to="/"
+              class="btn btn-primary p-4 dashboard-widget"
+            >
+              All Employees
+              <br>
+              <span class="badge badge-info">
+                {{totalEmployees}}
+              </span>
+            </router-link>
+          </div>
+          <div class="col-md-2">
+            <router-link
+              to="/"
+              class="btn btn-primary p-4 dashboard-widget"
+            >
+              Create New Table <i class="fa fa-plus-circle" />
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import apiService from "@/services/apiService";
+
+  export default {
+    name: "widgetList",  
+    data() {
+        return {
+            employees: []
+        };
+    },
+    computed: {
+        totalEmployees() {
+            return this.employees.length;  
+        }
+    },
+    created() {
+        this.retrieveEmployees();
+    },
+
+    methods: {
+      /*
+            function for get all employees
+            @param
+            @output: all employees
+        */
+        retrieveEmployees() {
+            apiService
+            .fetchEmployees()
+            .then((response) => {
+            this.employees = response.data;
+            return response.data;
+            })
+            .catch((error) => {
+            return error;
+            });
+        },
+    },
+  };
+  </script>
